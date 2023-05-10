54049545f5d5afe4b848afe198533dd35733fa25
benlow22 <ben2.2low@gmail.com>
2023-05-10 14:08:07 -0700

    add homepage

--- src/App.js
+++ src/App.js
@@ -12,10 +12,13 @@ import {
 	Route,
 } from "react-router-dom";
 
-
 // create router with JSX Route elements
 const appRouter = createBrowserRouter(
-	createRoutesFromElements(<Route path="/" element={<Root />} />)
+	createRoutesFromElements(
+		<Route path="/" element={<Root />}>
+			<Route index element={<HomePage />} />
+		</Route>
+	)
 );
 
 function App() {
